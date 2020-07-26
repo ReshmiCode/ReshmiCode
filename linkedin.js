7909dafcf6e2191f3fd0a22c3e526cebde18455b
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-07-26 15:35:14 -0500

    linkedin banner

---
+++ linkedin.js
@@ -0,0 +1,10 @@
+let reshmi = {
+  devExperience: ["Full Stack", "Mobile", "Quality Assurance"],
+  techLeadership: {
+    cohortLeader: "Rewriting The Code",
+    educationOfficer: "Association for Computing Machinery - UTD",
+    treasurer: "Women Mentoring Women In Engineering",
+  },
+  hobbies: ["hackathons", "graphic design", "learning"],
+  passion: "Introducing and engaging others in STEM",
+};
